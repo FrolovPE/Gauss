093e66a3519c2569536bad9a747a32006fb6375e
FrolovPE <UchFrolovP.com>
2025-10-23 14:16:19 -0400

    test make only terminal test from n=1 to n=999 and any variations of <m> and <s>

---
+++ supertest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $# != 2 ]; then
+	echo "Usage: test.sh <a.out> <r>"
+else
+    exe=$1
+    r=$2
+    
+    if [ -f $exe ];then
+    echo
+    else
+        echo "$exe doesn't exist"
+        exit
+    fi
+
+    for((i=2; i<1000; i++)); do
+
+        for((j=1; j<i; j++)); do
+
+            for((k=1; k<4;k++)); do
+                    echo "--------------------------------------------------------------------------"
+                    echo "$exe $i $j $r $k"
+                    $exe $i $j $r $k
+                    echo ""
+            done
+
+        done
+
+    done
+
+fi
